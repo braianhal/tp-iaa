cc7def8fd3af32e1a63c815608ec0e5ff0daa5b3
Ezequiel Borenstein <ezequiel.borenstein@gmail.com>
2018-06-15 02:17:01 -0300

    Agrego tests del token N_DIVIDE_N

--- src/test/java/ia/module/TokenTests.java
+++ src/test/java/ia/module/TokenTests.java
@@ -52,8 +52,20 @@ public class TokenTests extends TestCase{
         assertEquals(N_BY_N, parser.parse("4*log(2)").getToken().intValue());
         assertEquals(N_BY_N, parser.parse("4*log2b(2)").getToken().intValue());
 
+        assertEquals(N_BY_N, parser.parse("3^4").getToken().intValue());
+        assertEquals(N_BY_N, parser.parse("3^(4+2)").getToken().intValue());
+        assertEquals(N_BY_N, parser.parse("3^(4*2)").getToken().intValue());
+        assertEquals(N_BY_N, parser.parse("3^(4/2)").getToken().intValue());
+        assertEquals(N_BY_N, parser.parse("(3+2)^(4)").getToken().intValue());
+        assertEquals(N_BY_N, parser.parse("(1/2)^(4)").getToken().intValue());
+        assertEquals(N_BY_N, parser.parse("(1/2 + 1.5)^(4)").getToken().intValue());
+
         assertEquals(N_BY_N, parser.parse("4dx(2)").getToken().intValue());
         assertEquals(N_BY_N, parser.parse("4*dx(2)").getToken().intValue());
+        assertEquals(N_BY_N, parser.parse("(dx(2))^2").getToken().intValue());
+        assertEquals(N_BY_N, parser.parse("(dx(2x))^2").getToken().intValue());
+        assertEquals(N_BY_N, parser.parse("(dx(1+2x))^2").getToken().intValue());
+        assertEquals(N_BY_N, parser.parse("(dx((1+2x)+3+x))^2").getToken().intValue());
 
         assertEquals(N_BY_N, parser.parse("4int(0)").getToken().intValue());
         assertEquals(N_BY_N, parser.parse("4int(0+0)").getToken().intValue());
@@ -64,19 +76,6 @@ public class TokenTests extends TestCase{
         assertEquals(N_BY_N, parser.parse("4*int(0-0)").getToken().intValue());
         assertEquals(N_BY_N, parser.parse("4*int(0*0)").getToken().intValue());
         assertEquals(N_BY_N, parser.parse("4*int(0^2)").getToken().intValue());
-
-        assertEquals(N_BY_N, parser.parse("3^4").getToken().intValue());
-        assertEquals(N_BY_N, parser.parse("3^(4+2)").getToken().intValue());
-        assertEquals(N_BY_N, parser.parse("3^(4*2)").getToken().intValue());
-        assertEquals(N_BY_N, parser.parse("3^(4/2)").getToken().intValue());
-        assertEquals(N_BY_N, parser.parse("(3+2)^(4)").getToken().intValue());
-        assertEquals(N_BY_N, parser.parse("(1/2)^(4)").getToken().intValue());
-        assertEquals(N_BY_N, parser.parse("(1/2 + 1.5)^(4)").getToken().intValue());
-
-        assertEquals(N_BY_N, parser.parse("(dx(2))^2").getToken().intValue());
-        assertEquals(N_BY_N, parser.parse("(dx(2x))^2").getToken().intValue());
-        assertEquals(N_BY_N, parser.parse("(dx(1+2x))^2").getToken().intValue());
-        assertEquals(N_BY_N, parser.parse("(dx((1+2x)+3+x))^2").getToken().intValue());
 
         assertEquals(N_BY_N, parser.parse("(int(0))^2").getToken().intValue());
         assertEquals(N_BY_N, parser.parse("(int(0(1+2)))^2").getToken().intValue());
@@ -120,5 +119,52 @@ public class TokenTests extends TestCase{
         assertEquals(N_BY_X, parser.parse("(2^3)*int(2+3*4/3)").getToken().intValue());
         assertEquals(N_BY_X, parser.parse("(2*3)*int(2+3*4/3)").getToken().intValue());
         assertEquals(N_BY_X, parser.parse("(2+3*4/3)*int(2+3*4/3)").getToken().intValue());
+    }
+
+    public void testTokenNumberDivideNumber() throws Exception {
+        assertEquals(N_DIVIDED_N, parser.parse("0/2").getToken().intValue());
+        assertEquals(N_DIVIDED_N, parser.parse("(1+2)/(3+4)").getToken().intValue());
+        assertEquals(N_DIVIDED_N, parser.parse("2/(sqrt(5))").getToken().intValue());
+        assertEquals(N_DIVIDED_N, parser.parse("2/(sqrt(5))").getToken().intValue());
+        assertEquals(N_DIVIDED_N, parser.parse("(2*3)/(sqrt(5))").getToken().intValue());
+        assertEquals(N_DIVIDED_N, parser.parse("(2*3+sqrt(4)*ln(2))/2").getToken().intValue());
+
+        assertEquals(N_DIVIDED_N, parser.parse("4/sin(90)").getToken().intValue());
+        assertEquals(N_DIVIDED_N, parser.parse("4/cos(90)").getToken().intValue());
+        assertEquals(N_DIVIDED_N, parser.parse("4/tan(90)").getToken().intValue());
+        assertEquals(N_DIVIDED_N, parser.parse("4/ln(2)").getToken().intValue());
+        assertEquals(N_DIVIDED_N, parser.parse("4/log(2)").getToken().intValue());
+        assertEquals(N_DIVIDED_N, parser.parse("4/log2b(2)").getToken().intValue());
+        assertEquals(N_DIVIDED_N, parser.parse("log2b(2)/4").getToken().intValue());
+
+        assertEquals(N_DIVIDED_N, parser.parse("2/(3^4)").getToken().intValue());
+        assertEquals(N_DIVIDED_N, parser.parse("3/(3^(4+2))").getToken().intValue());
+        assertEquals(N_DIVIDED_N, parser.parse("4/(3^(4*2))").getToken().intValue());
+        assertEquals(N_DIVIDED_N, parser.parse("2/(3^(4/2))").getToken().intValue());
+        assertEquals(N_DIVIDED_N, parser.parse("5/((3+2)^(4))").getToken().intValue());
+        assertEquals(N_DIVIDED_N, parser.parse("1/((1/2)^(4))").getToken().intValue());
+        assertEquals(N_DIVIDED_N, parser.parse("2/((1/2 + 1.5)^(4))").getToken().intValue());
+        assertEquals(N_DIVIDED_N, parser.parse("((1/2 + 1.5)^(4))/2").getToken().intValue());
+
+        assertEquals(N_DIVIDED_N, parser.parse("4/dx(2)").getToken().intValue());
+        assertEquals(N_DIVIDED_N, parser.parse("4/dx(2)").getToken().intValue());
+        assertEquals(N_DIVIDED_N, parser.parse("2/((dx(2))^2)").getToken().intValue());
+        assertEquals(N_DIVIDED_N, parser.parse("1/((dx(2x))^2)").getToken().intValue());
+        assertEquals(N_DIVIDED_N, parser.parse("2/((dx(1+2x))^2)").getToken().intValue());
+        assertEquals(N_DIVIDED_N, parser.parse("3/((dx((1+2x)+3+x))^2)").getToken().intValue());
+        assertEquals(N_DIVIDED_N, parser.parse("((dx((1+2x)+3+x))^2)/3").getToken().intValue());
+
+        assertEquals(N_DIVIDED_N, parser.parse("4/int(0)").getToken().intValue());
+        assertEquals(N_DIVIDED_N, parser.parse("4/int(0+0)").getToken().intValue());
+        assertEquals(N_DIVIDED_N, parser.parse("4/int(0-0)").getToken().intValue());
+        assertEquals(N_DIVIDED_N, parser.parse("4/int(0*0)").getToken().intValue());
+        assertEquals(N_DIVIDED_N, parser.parse("4/int(0^2)").getToken().intValue());
+
+        assertEquals(N_DIVIDED_N, parser.parse("3/(int(0))^2").getToken().intValue());
+        assertEquals(N_DIVIDED_N, parser.parse("2/(int(0(1+2)))^2").getToken().intValue());
+        assertEquals(N_DIVIDED_N, parser.parse("1/(int(0*(1+2)))^2").getToken().intValue());
+        assertEquals(N_DIVIDED_N, parser.parse("2/(int(0*(1+x)))^2").getToken().intValue());
+        assertEquals(N_DIVIDED_N, parser.parse("3/(int((1+x)*0))^2").getToken().intValue());
+        assertEquals(N_DIVIDED_N, parser.parse("(int((1+x)*0))^2/4").getToken().intValue());
     }
 }
